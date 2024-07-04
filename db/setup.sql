62ec08230d7a489024f1abc4458542131772fcfa
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2024-07-04 18:58:48 -0300

    :sparkles: updated db variables location

--- db/setup.sql
+++ db/setup.sql
@@ -8,12 +8,8 @@ CREATE TABLE logs (
 	    message VARCHAR(200)
 );
 
-
--- Crie o usuário
 CREATE USER 'syslogserver'@'%' IDENTIFIED BY 'Syslog#123db';
 
--- Conceda privilégios ao usuário no banco de dados currentTS
 GRANT ALL PRIVILEGES ON syslogdb.* TO 'syslogserver'@'%';
 
--- Atualize os privilégios
 FLUSH PRIVILEGES;
