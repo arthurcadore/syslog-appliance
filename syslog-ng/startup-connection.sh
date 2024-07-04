609076276e9b50c62e6918459b5b285c753d87bf
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2024-07-04 20:28:37 -0300

    :sparkles: updated configuration to use supervisord

---
+++ syslog-ng/startup-connection.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+until mysqladmin ping -h"$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" &>/dev/null; do
+    echo "MySQL is unavailable - sleeping"
+    sleep 1
+done
+
+echo "MySQL is up - executing command"
