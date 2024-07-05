5f69499ce57b6129cf9097ed940e4efae32b384d
arthurcadore <arthurbarcella.ifsc@gmail.com>
2024-07-05 01:59:39 +0000

    :bug: fixed incorrect startup scripting

--- syslog-ng/startup-connection.sh
+++ syslog-ng/startup-connection.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while ! mysqladmin ping -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" --silent; do
+while ! mysqladmin ping -h"$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" --silent; do
     echo "Waiting for database be ready..."
     sleep 1
 done
@@ -8,4 +8,4 @@ done
 echo "MySQL is up - executing command"
     sleep 1
 
-syslog-ng -e
+syslog-ng -Fe
