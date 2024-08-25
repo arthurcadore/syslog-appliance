2bd5fb9d49f2eb088fe05a0d00e19c9418309885
arthurcadore <arthurbarcella.ifsc@gmail.com>
2024-08-25 14:40:11 +0000

    :sparkles: added IP_source column to main

--- db/setup.sql
+++ db/setup.sql
@@ -1,12 +1,13 @@
 USE syslogdb;
 
 CREATE TABLE logs (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    datetime VARCHAR(16),
-    host VARCHAR(32),
-    program VARCHAR(200),
-    pid VARCHAR(8),
-    message VARCHAR(200)
+	    id INT AUTO_INCREMENT PRIMARY KEY,
+	    datetime VARCHAR(16),
+	    host VARCHAR(32),
+	    source_ip VARCHAR(45),  
+	    program VARCHAR(200),
+	    pid VARCHAR(8),
+	    message VARCHAR(200)
 );
 
 CREATE USER 'syslogserver'@'%' IDENTIFIED BY 'Syslog#123db';
@@ -14,3 +15,4 @@ CREATE USER 'syslogserver'@'%' IDENTIFIED BY 'Syslog#123db';
 GRANT ALL PRIVILEGES ON syslogdb.* TO 'syslogserver'@'%';
 
 FLUSH PRIVILEGES;
+
