42a28f03126a4d35b22aae643e1f97cc2e9ac8ad
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2024-08-24 21:25:17 -0300

    :sparkles: updated repository

--- db/setup.sql
+++ db/setup.sql
@@ -1,11 +1,12 @@
 USE syslogdb;
 
 CREATE TABLE logs (
-	    datetime VARCHAR(16),
-	    host VARCHAR(32),
-	    program VARCHAR(200),
-	    pid VARCHAR(8),
-	    message VARCHAR(200)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    datetime VARCHAR(16),
+    host VARCHAR(32),
+    program VARCHAR(200),
+    pid VARCHAR(8),
+    message VARCHAR(200)
 );
 
 CREATE USER 'syslogserver'@'%' IDENTIFIED BY 'Syslog#123db';
